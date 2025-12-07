065a4409987fb95f374e1d6c5f4dfbf1f352fcb6
alexey_bazhenov <bazhenov.aleksey.s@gmail.com>
2025-12-07 18:16:17 +0200

    add first code

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
+
+class Config:
+    def __init__(self) -> None:
+        self.BOT_TOKEN = os.getenv("RECEIPT_SCANNER_BOT_TOKEN")
+
+config = Config()
